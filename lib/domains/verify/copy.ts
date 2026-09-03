344f690242bd059a6c5560ce55c25dcd2714fe40
lucas <lcs.carvajal@gmail.com>
2026-09-03 18:53:47 +0200

    update copy

--- lib/domains/verify/copy.ts
+++ lib/domains/verify/copy.ts
@@ -1,8 +1,8 @@
 export const VERIFY_SUCCESS_TITLE = "Email bestätigt";
 export const VERIFY_SUCCESS_BODY =
-  "Deine Email ist jetzt bestätigt. Die Bewertung wird in Kürze anonym veröffentlicht.";
+  "Deine Email ist jetzt bestätigt. Die Bewertung wird in Kürze anonym veröffentlicht. 😉";
 
-export const VERIFY_DEAD_LINK_TITLE = "Dieser Link funktioniert nicht";
+export const VERIFY_DEAD_LINK_TITLE = "Dieser Link funktioniert nicht :(";
 export const VERIFY_DEAD_LINK_BODY =
   "Dieser Link ist ungültig, abgelaufen oder wurde bereits verwendet.";
 
